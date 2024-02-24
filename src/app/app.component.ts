b4278b1abba6bf61ef03f301a13b2779f8e49e0b
RaniaAshraf1 <rraniaashraf77@gmail.com>
2024-02-24 14:56:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
