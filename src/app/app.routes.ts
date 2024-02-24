67634f374b4153046c4fae01156334391cdc5db1
ahmed reda <ahmedreda1009@gmail.com>
2024-02-24 19:34:39 +0200

    added payment ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PaymentComponent } from './components/payment/payment/payment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'checkout/payment', component: PaymentComponent },
+];
