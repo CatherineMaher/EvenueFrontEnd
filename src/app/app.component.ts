6b2e1049e75c5c7a3f34696f18b2793b3d1146de
RaniaAshraf1 <rraniaashraf77@gmail.com>
2024-02-26 00:29:19 +0200

    add event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CreateEventComponent } from './components/create-event/create-event.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"addEvent",component:CreateEventComponent},
+];
